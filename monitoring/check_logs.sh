a3787da4790b36db2b9d0e1845dd72415a5ecf22
rafael-abreu2812 <rafael.abreu2812@gmail.com>
2024-12-31 18:51:30 -0300

    fix: melhor visualização dos logs

--- monitoring/check_logs.sh
+++ monitoring/check_logs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+echo "--------------------------------------------------------------------------------------------------"
 
 #Checa se existe algum erro no container do NGINX
 docker logs nginx 2>&1 | grep -i error || echo "$(date) - Nenhum erro encontrado no NGINX!"
